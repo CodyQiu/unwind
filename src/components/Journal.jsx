dfeb6be45711fc7050e931efa77a61a0a1a15fc6
Cody Qiu <codyqiu2016@gmail.com>
2026-03-28 02:55:46 -0700

    added changes to journal + adjust breathing

--- src/components/Journal.jsx
+++ src/components/Journal.jsx
@@ -1,8 +1,13 @@
 function Journal() {
+  const [numEntries, setNumEntries] = useState(0);
+
+  useEffect(() => {}, [numEntries]);
+
   return (
     <div>
       <h2>Sleep Journal</h2>
-      <p>Record your thoughts and feelings about your sleep.</p>
+      <p>What's on your mind today?</p>
+      <textarea name="" id=""></textarea>
     </div>
   );
 }
